e0faa2411d827b24e20c5bdc806ed1ae857a45c9
Jack Matanky <JackMatanky@gmail.com>
2026-08-05 11:29:28 +0300

    refactor: replace TemplateName manual impls with string_newtype!

--- src/better_notes/models.rs
+++ src/better_notes/models.rs
@@ -36,60 +36,12 @@ use serde::{Deserialize, Serialize};
 
 use crate::zotero::ItemKey;
 
-/// Name of a Better Notes template, such as `"default"` or a custom template
-/// name.
-#[derive(
-    Clone,
-    Debug,
-    Default,
-    Eq,
-    Hash,
-    Ord,
-    PartialEq,
-    PartialOrd,
-    Deserialize,
-    Serialize,
-    JsonSchema,
-)]
-#[serde(transparent)]
-pub(crate) struct TemplateName(pub(crate) String);
-
-impl TemplateName {
-    /// Returns the template name as a string slice.
-    pub(crate) fn as_str(&self) -> &str {
-        &self.0
-    }
-}
-
-impl std::fmt::Display for TemplateName {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(&self.0)
-    }
-}
-
-impl From<String> for TemplateName {
-    fn from(value: String) -> Self {
-        Self(value)
-    }
-}
-
-impl From<&str> for TemplateName {
-    fn from(value: &str) -> Self {
-        Self(value.to_owned())
-    }
-}
-
-impl AsRef<str> for TemplateName {
-    fn as_ref(&self) -> &str {
-        &self.0
-    }
-}
-
-impl PartialEq<str> for TemplateName {
-    fn eq(&self, other: &str) -> bool {
-        self.0 == other
-    }
-}
+string_newtype!(
+    TemplateName,
+    "Name of a Better Notes template, such as `\"default\"` or a custom \
+     template name.",
+    json
+);
 
 /// Output format for Better Notes note export.
 #[derive(
